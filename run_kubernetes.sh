00932b04c0a8398d0c6f311b9f1e2f1c316bd7a0
jaysavy <john.s@cchubnigeria.com>
2023-03-01 17:01:32 +0100

    tweaked main folder

---
+++ run_kubernetes.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=oloruntobi/machine-learning-app
+
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+#kubectl run machine-learning-app --image=$dockerpath 
+kubectl run machine-learning-app --image=$dockerpath --port=80 --labels app=machine-learning-app 
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods 
+
+# Step 4:
+# Forward the container port to a host
+#kubectl port-forward ml-app 8080:80 
+kubectl port-forward machine-learning-app 8000:80
+
+ 
+
